e5bbaea481579359b32792b1d68ace17858d7f1a
Nitabushi <biglip.glasses0529@gmail.com>
2025-05-23 21:33:25 +0900

    上書きと追記の処理をwrite_to_fileで共通化

--- file_operations.py
+++ file_operations.py
@@ -1,34 +1,36 @@
 file_path = 'input.txt'
 
-# 元のファイルの内容に戻す関数
-def init_file():
-    with open(file_path, 'w', encoding='utf-8')as file:
-        file.write('こんにちは、私の名前は田中です。')
-        print('---ファイルを元の内容に戻しました---')
-    read_origin_file()
+def write_to_file(content: str, mode: str = 'w'):
+    """ファイルに書き込みまたは追記を行う"""
+    with open(file_path, mode, encoding='utf-8')as file:
+        file.write(content)
 
-# 元ファイルを表示する関数
-def read_origin_file():
+def read_file():
+    """ファイルの内容を表示"""
+    print('---ファイルの中身を表示---')
     with open(file_path, 'r', encoding='utf-8')as file:
-        print('---ファイルの中身を表示---')
         print(file.read())
 
-# ファイルの内容を上書きする関数
+def init_file():
+    """ファイルの内容を初期状態に戻す"""
+    write_to_file('こんにちは、私の名前は田中です。', mode='w')
+    print('---ファイルを元の内容に戻しました---')
+    read_file()
+
 def write_file():
-    with open(file_path, 'w', encoding='utf-8')as file:
-        file.write('ファイルを上書きしました。')
-        print('---ファイルを上書き---')
-    read_origin_file()
+    """ファイルの内容を上書きする"""
+    write_to_file('ファイルを上書きしました', mode='w')
+    print('---ファイルを上書き---')
+    read_file()
 
-# ファイルの内容を追記する関数
 def add_file():
-    with open(file_path, 'a', encoding='utf-8')as file:
-        file.write('ファイルに追記しました。')
-        print('---ファイルを追記---')
-    read_origin_file()
+    """ファイルの内容を追記する"""
+    write_to_file('ファイルに追記しました', mode='a')
+    print('---ファイルを追記---')
+    read_file()
 
-
-read_origin_file()
+# 実行
+read_file()
 write_file()
 add_file()
 init_file()
