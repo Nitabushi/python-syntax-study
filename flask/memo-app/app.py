7f7d44be1ea3daa7ad26a72b2b6f33e0049186f6
Nitabushi <biglip.glasses0529@gmail.com>
2025-05-24 16:07:20 +0900

    入力、確認、ほぞん画面を追加しました。また保存先ファイルを作成しました

--- flask/memo-app/app.py
+++ flask/memo-app/app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/confirm', methods=['POST'])
+def submit():
+    message = request.form['message']
+    return render_template('confirm.html',message=message)
+
+@app.route('/complete', methods=['POST'])
+def complete():
+    message = request.form['message']
+    file_path = "data/output.txt"
+    with open(file_path, "w", encoding="utf-8")as file:
+        file.write(message)
+    return render_template('complete.html', message=message)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
