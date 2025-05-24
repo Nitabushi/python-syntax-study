e4aec1e3bf3c5274994d9d7fdc8041adb5018724
Nitabushi <biglip.glasses0529@gmail.com>
2025-05-24 14:35:10 +0900

    ファイルのバリデーションチェック処理を追加。またファイル書き込み処理について、変数を外だしした。

--- user_input.py
+++ user_input.py
@@ -1,3 +1,9 @@
+file_path = "data/output.txt"
+ALLOWED_MODES =('w')
+
+# 使用するファイルのモード
+mode = 'w'
+
 # 情報を受け取る
 user_input = input("何か入力してください：")
 
@@ -5,9 +11,13 @@ user_input = input("何か入力してください：")
 if not user_input.strip():
     raise ValueError("入力が空です。不正な値です。")
 
-# 受け取った情報をファイルに書き込む
-with open("data/output.txt", "w", encoding="utf-8")as file:
-    file.write(user_input)
-print("入力内容を  output.txt  に保存しました。")
+# ファイルのバリデーション
+if mode not in ALLOWED_MODES:
+    raise ValueError(f"modeは{ALLOWED_MODES}のいずれかである必要があります。")
 
+# ファイルに書き込む
+with open(file_path, mode, encoding="utf-8")as file:
+    file.write(user_input)
+
+print("入力内容を  output.txt  に保存しました。")
 print(f"ユーザから入力された内容は「{user_input}」です。")
