373bcb3992ab19dd21b90efcca2753f7013b5207
Nitabushi <biglip.glasses0529@gmail.com>
2025-05-28 23:13:20 +0900

    Flaskアプリケーションにユーザー認証機能を追加し、ログインおよびログアウトのルートを実装。ホームページとログインページのテンプレートを作成。

--- flask/todo-app/app.py
+++ flask/todo-app/app.py
@@ -1,10 +1,52 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
-@app.route("/")
-def hello():
-    return "Hello ToDoApp!"
+# Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-if __name__ == "__main__":
+# テスト用のユーザIDとパスワード
+users = {'testuser': {'password': 'testpass'}}
+
+# ユーザークラス
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+# ユーザー読み込みコールバック
+@login_manager.user_loader
+def load_user(user_id):
+    if user_id in users:
+        return User(user_id)
+    return None
+
+@app.route('/')
+@login_required
+def home():
+    return render_template('home.html', name=current_user.id)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users and users[username]['password'] == password:
+            user = User(username)
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            flash('ログイン失敗：ユーザー名かパスワードが間違っています')
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8002, debug=True)
